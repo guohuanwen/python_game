72f19794f1bddcd1b99d4fa0dfab4f8bec9c1c83
huanwen guo <1179636441@qq.com>
2017-03-31 00:43:59 +0800

    跳跃

--- game/bird.py
+++ game/bird.py
@@ -6,17 +6,28 @@ import math
 
 class Bird(cocos.sprite.Sprite):
 
+    #重力加速度
+    gravity = -1.5
+    #起跳速度
+    jump_velocity = 15
+
     def __init__(self):
         super(Bird, self).__init__('apple.png', position=(0, 0))
         self.last_x = -1
         self.last_y = -1
         self.move_x = 0
         self.move_y = 0
-        self.velocity = 1.0
+        self.velocity = 3.0
         self.direction = 0.0
+        self.velocity_y = 0.0
+
+
+    def drop(self):
+        self.velocity_y = self.velocity_y + 0.5 * self.gravity
 
     def jump(self):
-        self.do(JumpBy((200, 0), 50, 3, 5))
+        self.velocity_y = self.jump_velocity
+
 
     def change_direction(self, key):
         if 65361 in key:  # 左
@@ -28,15 +39,20 @@ class Bird(cocos.sprite.Sprite):
         if 65364 in key:  # 下
             self.direction = math.pi * 3 / 2.0
 
+        if 32 in key:#空格
+            self.direction = 0.0
+            self.jump()
+
+
+
+
     def refresh(self):
-        print self.direction
+        self.drop()
         if self.direction < 0:
             return
-        if self.direction == 0:
-            self.x = self.x + self.velocity
         else:
             self.x = self.x + math.cos(self.direction) * self.velocity
-            self.y = self.y + math.sin(self.direction) * self.velocity
+            self.y = self.y + self.velocity_y
 
         if self.last_x < 0:
             self.last_x = self.x
